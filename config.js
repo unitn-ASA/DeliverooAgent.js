e84e8098f6ee12168a1731d6fb9ab8e7343e5fc2
Marco Robol <marco.robol@unitn.it>
2023-03-21 09:03:11 +0100

    refactorying lib

--- config.js
+++ config.js
@@ -1,5 +1,10 @@
-module.exports = {
-    host: 'https://deliveroojs.onrender.com',
-    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoibWFyY28iLCJpYXQiOjE2NzcxOTg1ODN9.SGs0A3hDpcGOpAZ6hqmpWhVgSSzsHB4-_bkIW3ILAI4' // valid on render
-    // token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoibWFyY28iLCJpYXQiOjE2NzcwNzU0MDl9.6a09tpwXmSdCrWVG2cYPnPaw_TwmfQucxWCwTf4fGec' // valid locally
-}
+// valid on render
+// let host = "https://deliveroojs.onrender.com";
+// let token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoibWFyY28iLCJpYXQiOjE2NzcxOTg1ODN9.SGs0A3hDpcGOpAZ6hqmpWhVgSSzsHB4-_bkIW3ILAI4'
+    
+// valid locally
+let host = "http://localhost:8080";
+let token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImRkODZlNDY4MDA3IiwibmFtZSI6Im1hcmNvIiwiaWF0IjoxNjc5MDcyNDQ0fQ.eh7mqMBW1457BBERE5Y7RJWDtHDz7--f5rQXXLulT1s'
+
+export { host };
+export { token };
