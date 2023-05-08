a1fa9a81ba7eeac3b6c46d25ed93c31409c5c4b8
Marco Robol <marco.robol@unitn.it>
2023-05-08 22:22:02 +0200

    benchmark agent

---
+++ benchmarkAgent/benchmarkAgent.js
@@ -0,0 +1,548 @@
+import { DeliverooApi } from "@unitn-asa/deliveroo-js-client";
+import depth_search_daemon from "./depth_search_daemon.js";
+
+const client = new DeliverooApi(
+    'http://localhost:8080',
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjA5ZmQ2NDllNzZlIiwibmFtZSI6Im1hcmNvIiwiaWF0IjoxNjc5OTk3Njg2fQ.6_zmgL_C_9QgoOX923ESvrv2i2_1bgL_cWjMw4M7ah4'
+)
+const depth_search = depth_search_daemon(client);
+
+function distance( {x:x1, y:y1}, {x:x2, y:y2}) {
+    const dx = Math.abs( Math.round(x1) - Math.round(x2) )
+    const dy = Math.abs( Math.round(y1) - Math.round(y2) )
+    return dx + dy;
+}
+
+
+
+/**
+ * Beliefset revision function
+ */
+const me = { carrying: new Map() };
+client.onYou( ( {id, name, x, y, score} ) => {
+    me.id = id
+    me.name = name
+    me.x = x
+    me.y = y
+    me.score = score
+} )
+const parcels = new Map();
+client.onParcelsSensing( async ( perceived_parcels ) => {
+    for (const p of perceived_parcels) {
+        parcels.set( p.id, p)
+        if ( p.carriedBy == me.id ) {
+            me.carrying.set( p.id, p );
+        }
+    }
+    for ( const [id,p] of parcels.entries() ) {
+        if ( ! perceived_parcels.find( p=>p.id==id ) ) {
+            parcels.delete( id ); 
+            me.carrying.delete( id );
+        }
+    }
+} )
+
+var AGENTS_OBSERVATION_DISTANCE
+var MOVEMENT_DURATION
+var PARCEL_DECADING_INTERVAL
+client.onConfig( (config) => {
+    AGENTS_OBSERVATION_DISTANCE = config.AGENTS_OBSERVATION_DISTANCE;
+    MOVEMENT_DURATION = config.MOVEMENT_DURATION;
+    PARCEL_DECADING_INTERVAL = config.PARCEL_DECADING_INTERVAL == '1s' ? 1000 : 1000000;
+} );
+
+const map = {
+    width:undefined,
+    height:undefined,
+    tiles: new Map(),
+    add: function ( tile ) {
+        const {x, y} = tile;
+        return this.tiles.set( x+1000*y, tile );
+    },
+    xy: function (x, y) {
+        return this.tiles.get( x+1000*y )
+    }
+};
+client.onMap( (width, height, tiles) => {
+    map.width = width;
+    map.height = height;
+    for (const t of tiles) {
+        map.add( t );
+    }
+} )
+client.onTile( (x, y, delivery) => {
+    map.add( {x, y, delivery} );
+} )
+
+
+
+/**
+ * Options generation and filtering function
+ */
+client.onParcelsSensing( parcels => {
+
+    // TODO revisit beliefset revision so to trigger option generation only in the case a new parcel is observed
+
+    /**
+     * Options generation
+     */
+    const options = []
+    for (const parcel of parcels.values()) {
+        if ( ! parcel.carriedBy )
+            options.push( [ 'go_pick_up', parcel.x, parcel.y, parcel.id, parcel.reward ] );
+    }
+    let carriedQty = me.carrying.size;
+    let carriedReward = Array.from( me.carrying.values() ).reduce( (acc, parcel) => acc + parcel.reward, 0 )
+    let nearestDelivery = Array.from( map.tiles.values() ).filter( ({delivery}) => delivery ).sort( (a,b) => distance(a,me)-distance(b,me) )[0]
+    if ( carriedReward > 0 ) {
+        options.push( [ 'go_deliver', nearestDelivery.x, nearestDelivery.y ] );
+    }
+    
+    /**
+     * Options filtering
+     */
+    let best_option;
+    let best_final_reward = 0; // parcel value - cost for pick up - cost for delivery  //Number.MAX_VALUE;
+    for (const option of options) {
+        if ( option[0] == 'go_pick_up' ) {
+            let [go_pick_up,x,y,id,reward] = option;
+            let current_reward = carriedReward + reward - (carriedQty+1) * MOVEMENT_DURATION/PARCEL_DECADING_INTERVAL * (distance( {x, y}, me ) + distance( {x, y}, nearestDelivery ) ); // parcel value - cost for pick up - cost for delivery
+            if ( current_reward > best_final_reward ) {
+                best_option = option;
+                best_final_reward = current_reward;
+            }
+        }
+        if ( option[0] == 'go_deliver' ) {
+            let [go_pick_up,x,y] = option;
+            let current_reward = carriedReward - carriedQty * MOVEMENT_DURATION/PARCEL_DECADING_INTERVAL * distance( me, nearestDelivery ); // carried parcels value - cost for delivery
+            if ( current_reward > best_final_reward ) {
+                best_option = option;
+                best_final_reward = current_reward;
+            }
+        }
+    }
+
+    /**
+     * Best option is selected
+     */
+    if ( best_option )
+        myAgent.push( best_option )
+
+} )
+// client.onAgentsSensing( agentLoop )
+// client.onYou( agentLoop )
+
+
+
+/**
+ * Intention
+ */
+class Intention {
+
+    // Plan currently used for achieving the intention 
+    #current_plan;
+    
+    // This is used to stop the intention
+    #stopped = false;
+    get stopped () {
+        return this.#stopped;
+    }
+    stop () {
+        // this.log( 'stop intention', ...this.#predicate );
+        this.#stopped = true;
+        if ( this.#current_plan)
+            this.#current_plan.stop();
+    }
+
+    /**
+     * #parent refers to caller
+     */
+    #parent;
+
+    /**
+     * predicate is in the form ['go_to', x, y]
+     */
+    get predicate () {
+        return this.#predicate;
+    }
+    #predicate;
+
+    constructor ( parent, predicate ) {
+        this.#parent = parent;
+        this.#predicate = predicate;
+    }
+
+    log ( ...args ) {
+        if ( this.#parent && this.#parent.log )
+            this.#parent.log( '\t', ...args )
+        else
+            console.log( ...args )
+    }
+
+    #started = false;
+    /**
+     * Using the plan library to achieve an intention
+     */
+    async achieve () {
+        // Cannot start twice
+        if ( this.#started)
+            return this;
+        else
+            this.#started = true;
+
+        // Trying all plans in the library
+        for (const planClass of planLibrary) {
+
+            // if stopped then quit
+            if ( this.stopped ) throw [ 'stopped intention', ...this.predicate ];
+
+            // if plan is 'statically' applicable
+            if ( planClass.isApplicableTo( ...this.predicate ) ) {
+                // plan is instantiated
+                this.#current_plan = new planClass(this.parent);
+                this.log('achieving intention', ...this.predicate, 'with plan', planClass.name);
+                // and plan is executed and result returned
+                try {
+                    const plan_res = await this.#current_plan.execute( ...this.predicate );
+                    this.log( 'succesful intention', ...this.predicate, 'with plan', planClass.name, 'with result:', plan_res );
+                    return plan_res
+                // or errors are caught so to continue with next plan
+                } catch (error) {
+                    this.log( 'failed intention', ...this.predicate,'with plan', planClass.name, 'with error:', error );
+                }
+            }
+
+        }
+
+        // if stopped then quit
+        if ( this.stopped ) throw [ 'stopped intention', ...this.predicate ];
+
+        // no plans have been found to satisfy the intention
+        // this.log( 'no plan satisfied the intention ', ...this.predicate );
+        throw ['no plan satisfied the intention ', ...this.predicate ]
+    }
+
+}
+
+/**
+ * Plan library
+ */
+const planLibrary = [];
+
+class Plan {
+
+    // This is used to stop the plan
+    #stopped = false;
+    stop () {
+        // this.log( 'stop plan' );
+        this.#stopped = true;
+        for ( const i of this.#sub_intentions ) {
+            i.stop();
+        }
+    }
+    get stopped () {
+        return this.#stopped;
+    }
+
+    /**
+     * #parent refers to caller
+     */
+    #parent;
+
+    constructor ( parent ) {
+        this.#parent = parent;
+    }
+
+    log ( ...args ) {
+        if ( this.#parent && this.#parent.log )
+            this.#parent.log( '\t', ...args )
+        else
+            console.log( ...args )
+    }
+
+    // this is an array of sub intention. Multiple ones could eventually being achieved in parallel.
+    #sub_intentions = [];
+
+    async subIntention ( predicate ) {
+        const sub_intention = new Intention( this, predicate );
+        this.#sub_intentions.push( sub_intention );
+        return await sub_intention.achieve();
+    }
+
+}
+
+class GoPickUp extends Plan {
+
+    static isApplicableTo ( go_pick_up, x, y, id ) {
+        return go_pick_up == 'go_pick_up';
+    }
+
+    async execute ( go_pick_up, x, y ) {
+        if ( this.stopped ) throw ['stopped']; // if stopped then quit
+        await this.subIntention( ['go_to', x, y] );
+        if ( this.stopped ) throw ['stopped']; // if stopped then quit
+        await client.pickup()
+        if ( this.stopped ) throw ['stopped']; // if stopped then quit
+        return true;
+    }
+
+}
+
+class GoDeliver extends Plan {
+
+    static isApplicableTo ( go_deliver, x, y ) {
+        return go_deliver == 'go_deliver';
+    }
+
+    async execute ( go_deliver, x, y ) {
+        if ( this.stopped ) throw ['stopped']; // if stopped then quit
+        await this.subIntention( ['go_to', x, y] );
+        if ( this.stopped ) throw ['stopped']; // if stopped then quit
+        await client.putdown()
+        if ( this.stopped ) throw ['stopped']; // if stopped then quit
+        return true;
+    }
+
+}
+
+class Patrolling extends Plan {
+
+    static isApplicableTo ( patrolling ) {
+        return patrolling == 'patrolling';
+    }
+
+    async execute ( patrolling ) {
+        if ( this.stopped ) throw ['stopped']; // if stopped then quit
+        let i = Math.round( Math.random() * map.tiles.size );
+        let tile = Array.from( map.tiles.values() ).at( i );
+        if ( tile )
+            await this.subIntention( ['go_to', tile.x, tile.y] );
+        if ( this.stopped ) throw ['stopped']; // if stopped then quit
+        return true;
+    }
+
+}
+
+class DepthSearchMove extends Plan {
+
+    static isApplicableTo ( go_to, x, y ) {
+        return go_to == 'go_to';
+    }
+
+    async execute ( go_to, x, y ) {
+
+        const plan = depth_search(x, y)
+
+        for ( const step of plan ) {
+
+            if ( this.stopped ) throw ['stopped']; // if stopped then quit
+            
+            const status = await client.move( step.action )
+            me.x = status.x;
+            me.y = status.y;
+            
+            if ( ! status ) {
+                this.log('stucked');
+                throw 'stucked';
+            }
+
+        }
+        
+        if ( me.x == x && me.y == y ) {
+            return true;
+        }
+        
+        this.log('target not reached');
+        throw 'target not reached';
+
+    }
+}
+
+class BlindMove extends Plan {
+
+    static isApplicableTo ( go_to, x, y ) {
+        return go_to == 'go_to';
+    }
+
+    async execute ( go_to, x, y ) {
+
+        while ( me.x != x || me.y != y ) {
+
+            if ( this.stopped ) throw ['stopped']; // if stopped then quit
+
+            let status_x = false;
+            let status_y = false;
+            
+            // this.log('me', me, 'xy', x, y);
+
+            if ( x > me.x )
+                status_x = await client.move('right')
+                // status_x = await this.subIntention( 'go_to', {x: me.x+1, y: me.y} );
+            else if ( x < me.x )
+                status_x = await client.move('left')
+                // status_x = await this.subIntention( 'go_to', {x: me.x-1, y: me.y} );
+
+            if (status_x) {
+                me.x = status_x.x;
+                me.y = status_x.y;
+            }
+
+            if ( this.stopped ) throw ['stopped']; // if stopped then quit
+
+            if ( y > me.y )
+                status_y = await client.move('up')
+                // status_x = await this.subIntention( 'go_to', {x: me.x, y: me.y+1} );
+            else if ( y < me.y )
+                status_y = await client.move('down')
+                // status_x = await this.subIntention( 'go_to', {x: me.x, y: me.y-1} );
+
+            if (status_y) {
+                me.x = status_y.x;
+                me.y = status_y.y;
+            }
+            
+            if ( ! status_x && ! status_y) {
+                this.log('stucked');
+                throw 'stucked';
+            } else if ( me.x == x && me.y == y ) {
+                // this.log('target reached');
+            }
+            
+        }
+
+        return true;
+
+    }
+}
+
+// plan classes are added to plan library 
+planLibrary.push( GoPickUp )
+planLibrary.push( Patrolling )
+planLibrary.push( GoDeliver )
+planLibrary.push( DepthSearchMove )
+planLibrary.push( BlindMove )
+
+
+
+
+
+
+
+/**
+ * Intention revision loop
+ */
+class IntentionRevision {
+
+    #intention_queue = new Array();
+    get intention_queue () {
+        return this.#intention_queue;
+    }
+
+    async loop ( ) {
+        while ( true ) {
+            // Consumes intention_queue if not empty
+            if ( this.intention_queue.length > 0 ) {
+                console.log( 'intentionRevision.loop', this.intention_queue.map(i=>i.predicate) );
+            
+                // Current intention
+                const intention = this.intention_queue[0];
+                
+                // Is queued intention still valid? Do I still want to achieve it?
+                // TODO this hard-coded implementation is an example
+                if ( intention.predicate[0] == "go_pick_up" ) {
+                    let id = intention.predicate[3]
+                    let p = parcels.get(id)
+                    if ( p && p.carriedBy ) {
+                        console.log( 'Skipping intention because no more valid', intention.predicate );
+
+                        // Remove from the queue
+                        this.intention_queue.shift();
+                        
+                        continue;
+                    }
+                }
+
+                // Start achieving intention
+                await intention.achieve()
+                // Catch eventual error and continue
+                .catch( error => {
+                    console.log( 'Failed intention', ...intention.predicate, 'with error:', error )
+                } );
+
+                // Remove from the queue
+                this.intention_queue.shift();
+            }
+            else {
+                this.push( this.idle );
+            }
+            // Postpone next iteration at setImmediate
+            await new Promise( res => setImmediate( res ) );
+        }
+    }
+
+    // async push ( predicate ) { }
+
+    log ( ...args ) {
+        console.log( ...args )
+    }
+
+}
+
+class IntentionRevisionQueue extends IntentionRevision {
+
+    async push ( predicate ) {
+        
+        // Check if already queued
+        if ( this.intention_queue.find( (i) => i.predicate.join(' ') == predicate.join(' ') ) )
+            return; // intention is already queued
+
+        console.log( 'IntentionRevisionReplace.push', predicate );
+        const intention = new Intention( this, predicate );
+        this.intention_queue.push( intention );
+    }
+
+}
+
+class IntentionRevisionReplace extends IntentionRevision {
+
+    async push ( predicate ) {
+
+        // Check if already queued
+        const last = this.intention_queue.at( this.intention_queue.length - 1 );
+        if ( last && last.predicate.join(' ') == predicate.join(' ') ) {
+            return; // intention is already being achieved
+        }
+        
+        console.log( 'IntentionRevisionReplace.push', predicate );
+        const intention = new Intention( this, predicate );
+        this.intention_queue.push( intention );
+        
+        // Force current intention stop 
+        if ( last ) {
+            last.stop();
+        }
+    }
+
+}
+
+class IntentionRevisionRevise extends IntentionRevision {
+
+    async push ( predicate ) {
+        console.log( 'Revising intention queue. Received', ...predicate );
+        // TODO
+        // - order intentions based on utility function (reward - cost) (for example, parcel score minus distance)
+        // - eventually stop current one
+        // - evaluate validity of intention
+    }
+
+}
+
+/**
+ * Start intention revision loop
+ */
+
+// const myAgent = new IntentionRevisionQueue();
+const myAgent = new IntentionRevisionReplace();
+myAgent.idle = [ "patrolling" ];
+// const myAgent = new IntentionRevisionRevise();
+myAgent.loop();
---
+++ benchmarkAgent/depth_search_daemon.js
@@ -0,0 +1,122 @@
+import { DeliverooApi } from "@unitn-asa/deliveroo-js-client";
+
+function distance( {x:x1, y:y1}, {x:x2, y:y2}) {
+    const dx = Math.abs( Math.round(x1) - Math.round(x2) )
+    const dy = Math.abs( Math.round(y1) - Math.round(y2) )
+    return dx + dy;
+}
+
+export default function ( /**@type {DeliverooApi}*/client ) {
+    
+    var AGENTS_OBSERVATION_DISTANCE
+    var MOVEMENT_DURATION
+    client.onConfig( (config) => {
+        AGENTS_OBSERVATION_DISTANCE = config.AGENTS_OBSERVATION_DISTANCE;
+        MOVEMENT_DURATION = config.MOVEMENT_DURATION;
+    } );
+    
+    /**
+     * @type {Map<x,Map<y,{x,y,delivery}>}
+     */
+    const map = new Map()
+    client.onTile( ( x, y, delivery ) => {
+        if ( ! map.has(x) )
+            map.set(x, new Map())    
+        map.get(x).set(y, {x, y, delivery})
+    } );
+    
+    var me = {};
+    client.onYou( ( {x, y} ) => {
+        me.x = x;
+        me.y = y;
+    } );
+
+    const agents = new Map()
+    client.onAgentsSensing( ( sensed_agents ) => {
+        for ( const {id, x, y} of sensed_agents ) {
+            agents.set(id, {x, y} );
+        }
+        for ( const [id, {x, y}] of agents.entries() ) {
+            if ( distance (me, {x, y}) < AGENTS_OBSERVATION_DISTANCE && ! sensed_agents.find( (sensed) => id != sensed.id ) )
+            agents.delete(id);
+        }
+    } );
+
+    return function (target_x, target_y) {
+        
+        for ( const {id, x, y} of agents.values() ) {
+            map.get(Math.ceil(x)).get(Math.ceil(y)).locked = true;
+            map.get(Math.floor(x)).get(Math.floor(y)).locked = true;
+        }
+
+        console.log('go from', me.x, me.y, 'to', target_x, target_y);
+
+        function search (cost, x, y, previous_tile, action_from_previous) {
+
+            if( ! map.has(x) || ! map.get(x).has(y) || map.get(x).get(y).locked )
+                return false;
+            
+            const tile = map.get(x).get(y)
+            if( tile.cost_to_here <= cost)
+                return false;
+            else {
+                tile.cost_to_here = cost;
+                tile.previous_tile = previous_tile;
+                if( action_from_previous )
+                    tile.action_from_previous = action_from_previous;
+            }
+            
+            if ( target_x == x && target_y == y ) {
+                // console.log('found with cost', cost)
+                // function backward ( tile ) {
+                //     console.log( tile.cost_to_here + ' move ' + tile.action_from_previous + ' ' + tile.x + ',' + tile.y );
+                //     if ( tile.previous_tile ) backward( tile.previous_tile );
+                // }
+                // backward( tile )
+                return true;
+            }
+
+            let options = new Array(
+                [cost+1, x+1, y, tile, 'right'],
+                [cost+1, x-1, y, tile, 'left'],
+                [cost+1, x, y+1, tile, 'up'],
+                [cost+1, x, y-1, tile, 'down']
+            );
+            options = options.sort( (a, b) => {
+                return distance({x: target_x, y: target_y}, {x: a[1], y: a[2]}) - distance({x: target_x, y: target_y}, {x: b[1], y: b[2]})
+            } )
+
+            search( ...options[0] )
+            search( ...options[1] )
+            search( ...options[2] )
+            search( ...options[3] )
+            
+        }
+
+        search(0, me.x, me.y);
+
+        let dest = map.get(target_x).get(target_y);
+        
+        const plan = [];
+
+        while ( dest.previous_tile ) {
+            // console.log( dest.cost_to_here + ' move ' + dest.action_from_previous + ' ' + dest.x + ',' + dest.y );
+            plan.unshift( { step: dest.cost_to_here, action: dest.action_from_previous, current: {x: dest.x, y: dest.y} } )
+            dest = dest.previous_tile;
+        }
+        
+        map.forEach( (map, x) => map.forEach( (tile, y) => {
+            delete tile.cost_to_here;
+            delete tile.previous_tile;
+            delete tile.action_from_previous;
+            delete tile.locked;
+        } ) )
+
+        return plan;
+
+    }
+
+}
+
+
+
