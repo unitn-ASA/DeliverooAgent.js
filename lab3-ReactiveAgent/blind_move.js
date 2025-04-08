53d03d6cb9a2ce9de09e96cc92b46404b3c30d02
Marco Robol <marco.robol@unitn.it>
2025-04-08 15:27:48 +0200

    fixes after today class

--- lab3-ReactiveAgent/blind_move.js
+++ lab3-ReactiveAgent/blind_move.js
@@ -2,7 +2,11 @@ import { DeliverooApi } from "@unitn-asa/deliveroo-js-client";
 
 const client = new DeliverooApi(
     'https://deliveroojs25.azurewebsites.net',
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjJjOTQyMSIsIm5hbWUiOiJtYXJjbyIsInRlYW1JZCI6IjViMTVkMSIsInRlYW1OYW1lIjoiZGlzaSIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNzQyNTY3NDE4fQ.5m8St0OZo_DCXCriYkLtsguOm1e20-IAN2JNgXL7iUQ'
+    // 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjJjOTQyMSIsIm5hbWUiOiJtYXJjbyIsInRlYW1JZCI6IjViMTVkMSIsInRlYW1OYW1lIjoiZGlzaSIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNzQyNTY3NDE4fQ.5m8St0OZo_DCXCriYkLtsguOm1e20-IAN2JNgXL7iUQ'
+    // 'https://deliveroojs2.rtibdi.disi.unitn.it/',
+    // 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImQyNmQ1NyIsIm5hbWUiOiJtYXJjbyIsInRlYW1JZCI6ImM3ZjgwMCIsInRlYW1OYW1lIjoiZGlzaSIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNzQwMDA3NjIwfQ.1lfKRxSSwj3_a4fWnAV44U1koLrphwLkZ9yZnYQDoSw'
+    // 'http://localhost:8080',
+    // 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjRiZDg3MyIsIm5hbWUiOiJtYXJjbyIsInRlYW1JZCI6IjA3ZmU2MiIsInRlYW1OYW1lIjoiZGlzaSIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNzM4NjAzNjMwfQ.Q9btNkm3VLXsZDsNHYsQm2nGUVfFnF-TWZrz4zPaWM4'
 )
 
 function distance( {x:x1, y:y1}, {x:x2, y:y2}) {
--- lab3-ReactiveAgent/one_move.js
+++ lab3-ReactiveAgent/one_move.js
@@ -2,7 +2,11 @@ import { DeliverooApi } from "@unitn-asa/deliveroo-js-client";
 
 const client = new DeliverooApi(
     'https://deliveroojs25.azurewebsites.net',
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjJjOTQyMSIsIm5hbWUiOiJtYXJjbyIsInRlYW1JZCI6IjViMTVkMSIsInRlYW1OYW1lIjoiZGlzaSIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNzQyNTY3NDE4fQ.5m8St0OZo_DCXCriYkLtsguOm1e20-IAN2JNgXL7iUQ'
+    // 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjJjOTQyMSIsIm5hbWUiOiJtYXJjbyIsInRlYW1JZCI6IjViMTVkMSIsInRlYW1OYW1lIjoiZGlzaSIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNzQyNTY3NDE4fQ.5m8St0OZo_DCXCriYkLtsguOm1e20-IAN2JNgXL7iUQ'
+    // 'https://deliveroojs2.rtibdi.disi.unitn.it/',
+    // 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImQyNmQ1NyIsIm5hbWUiOiJtYXJjbyIsInRlYW1JZCI6ImM3ZjgwMCIsInRlYW1OYW1lIjoiZGlzaSIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNzQwMDA3NjIwfQ.1lfKRxSSwj3_a4fWnAV44U1koLrphwLkZ9yZnYQDoSw'
+    // 'http://localhost:8080',
+    // 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjRiZDg3MyIsIm5hbWUiOiJtYXJjbyIsInRlYW1JZCI6IjA3ZmU2MiIsInRlYW1OYW1lIjoiZGlzaSIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNzM4NjAzNjMwfQ.Q9btNkm3VLXsZDsNHYsQm2nGUVfFnF-TWZrz4zPaWM4'
 )
 
 function distance( {x:x1, y:y1}, {x:x2, y:y2}) {
@@ -21,29 +25,40 @@ client.onYou( ( {id, name, x, y, score} ) => {
     me.score = score
 } )
 
-const db = new Map()
+var control = false;
 
 client.onParcelsSensing( async ( parcels ) => {
-    
+
     console.log( `me(${me.x},${me.y})`,
+        control ? 'skip' : 'go to parcels: ',
         parcels
         .map( p => `${p.reward}@(${p.x},${p.y})` )
         .join( ' ' )
     );
 
+    if ( control ) {
+        return;
+    }
+    control = true;
+    
     for ( let p of parcels ) {
         if ( ! p.carriedBy ) {
-            if ( me.x < p.x )
+            if      ( me.x == p.x-1 && me.y == p.y )
                 await client.emitMove('right');
-            else if ( me.x > p.x )
+            else if ( me.x == p.x+1 && me.y == p.y )
                 await client.emitMove('left')
-            else if ( me.y < p.y )
+            else if ( me.y == p.y-1 && me.x == p.x )
                 await client.emitMove('up')
-            else if ( me.y > p.y )
+            else if ( me.y == p.y+1 && me.x == p.x )
                 await client.emitMove('down')
-            client.emitPickup();
+
+            if ( me.x == p.x && me.y == p.y ) {
+                await client.emitPickup();
+            }
         }
     }
+    
+    control = false;
 
 } )
 
